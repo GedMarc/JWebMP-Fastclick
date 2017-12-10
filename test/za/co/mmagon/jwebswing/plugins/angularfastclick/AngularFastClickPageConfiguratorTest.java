29f51298ed690cb0df6a4617f6e2fd68c0a2bb3c
Marc Magon <ged_marc@hotmail.com>
2017-12-10 03:17:32 +0200

    Javadoc Updates

---
+++ test/za/co/mmagon/jwebswing/plugins/angularfastclick/AngularFastClickPageConfiguratorTest.java
@@ -0,0 +1,14 @@
+package za.co.mmagon.jwebswing.plugins.angularfastclick;
+
+import org.junit.jupiter.api.Test;
+import za.co.mmagon.jwebswing.Page;
+
+public class AngularFastClickPageConfiguratorTest
+{
+	@Test
+	public void testFastClickImplementation()
+	{
+		Page p = new Page();
+		System.out.println(p.toString(0));
+	}
+}
