4d52375783e3ea32a9570f6d3acae3672d21548a
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:51:24 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/angularfastclick/AngularFastClickPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularfastclick/AngularFastClickPageConfigurator.java
@@ -35,12 +35,42 @@ import javax.validation.constraints.NotNull;
 public class AngularFastClickPageConfigurator
 		implements IPageConfigurator
 {
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
+
 	/*
 	 * Constructs a new AngularAutoExpandPageConfigurator
 	 */
 	public AngularFastClickPageConfigurator()
 	{
 		//Nothing needed
+	}
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return AngularFastClickPageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		AngularFastClickPageConfigurator.enabled = mustEnable;
 	}
 
 	@NotNull
@@ -55,5 +85,11 @@ public class AngularFastClickPageConfigurator
 			    .addJavaScriptReference(new JavascriptReference("FastClickReferenceJS", 4.003, "bower_components/ng-fastclick/dist/index.min.js", 11));
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return AngularFastClickPageConfigurator.enabled;
 	}
 }
