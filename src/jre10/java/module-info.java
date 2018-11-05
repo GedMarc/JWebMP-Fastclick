82805811bee6c577e2c81f05662aa8a587985912
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:36 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,7 +1,10 @@
 import com.jwebmp.core.base.angular.services.IAngularModule;
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.angularfastclick.AngularFastClickModule;
 import com.jwebmp.plugins.angularfastclick.AngularFastClickPageConfigurator;
+import com.jwebmp.plugins.angularfastclick.implementations.AngularFastClickExclusionsModule;
 
 module com.jwebmp.plugins.angularfastclick {
 	exports com.jwebmp.plugins.angularfastclick;
@@ -12,8 +15,12 @@ module com.jwebmp.plugins.angularfastclick {
 
 	requires java.validation;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with AngularFastClickPageConfigurator;
 	provides IAngularModule with AngularFastClickModule;
+
+	provides IGuiceScanJarExclusions with AngularFastClickExclusionsModule;
+	provides IGuiceScanModuleExclusions with AngularFastClickExclusionsModule;
 
 }
