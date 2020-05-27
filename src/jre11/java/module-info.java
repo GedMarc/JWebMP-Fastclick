a73caf93c0e026c71b4ce1ded6240c9ac7d1112a
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:31:56 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,7 +3,6 @@ module com.jwebmp.plugins.angularfastclick {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
